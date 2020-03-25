a6914bd14490a64abef6d5931db0723a72fd2a4d
郵差 <a34007024@gmail.com>
2020-03-25 12:03:56 +0800

    Create printStarPyramid.java

---
+++ src/edu/NTUNHS/luckypig/printStarPyramid.java
@@ -0,0 +1,22 @@
+package edu.NTUNHS.luckypig;
+import java.util.*;
+/**
+ * printStarPyramid
+ */
+public class printStarPyramid {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("請輸入要幾層三角形:");
+        drawTriangle(sc1.nextInt());
+        sc1.close();
+    }
+
+    private static void drawTriangle(int count) {
+        int i, j;
+        for ( i = 1; i <= count; i++ ) {
+           for ( j = 1; j <= i; j++ )
+              System.out.print("*");
+           System.out.print("\n");
+        }
+    }
+}
