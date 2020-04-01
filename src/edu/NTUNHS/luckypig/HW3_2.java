33cb65b32827f95642b41585b5999f89aaf1b7bc
郵差 <a34007024@gmail.com>
2020-04-01 16:40:54 +0800

    Update HW3_2.java
    費氏數列完工~

--- src/edu/NTUNHS/luckypig/HW3_2.java
+++ src/edu/NTUNHS/luckypig/HW3_2.java
@@ -7,17 +7,21 @@ import java.util.Scanner;
  */
 public class HW3_2 {
     public static void main(String[] args) {
-        int[] f = {0,1,1,2,3,5,8,13,21,34};
+        long[] f = new long[100];
+        f[0] = 0;f[1] = 1;
         System.out.println("請輸入一個數");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
         if(n < 0)System.out.println("請輸入大於0的整數");
-        else if(n <= 10){
-            System.out.println(f[n-1]);
+        else if(n <= 92){
+            for(int i=2;i<=n;i++){
+                f[i] = f[i-1] + f[i-2];
+            }
+            System.out.println(f[n]);
         }
         else{
-            
+            System.out.println("超過Long能儲存的範圍囉~ 數字請輸入小一點的謝謝!");
         }
     }
 }
