58a4df17573a5c75dbaf13e92790125718b249d8
郵差 <a34007024@gmail.com>
2020-04-08 12:14:07 +0800

    Create CH03_13_practice.java

---
+++ src/edu/NTUNHS/luckypig/CH03_13_practice.java
@@ -0,0 +1,9 @@
+package edu.NTUNHS.luckypig;
+
+public class CH03_13_practice {
+    public static void main(String[] args) {
+        for(int i=1;i<100;i++){
+            if(i%3 == 0)System.out.println(i);
+        }
+    }
+}
