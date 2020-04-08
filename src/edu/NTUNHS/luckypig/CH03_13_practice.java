97863721d23093013dec89f2b8b89d090b9def90
郵差 <a34007024@gmail.com>
2020-04-08 12:21:01 +0800

    use continue in for

--- src/edu/NTUNHS/luckypig/CH03_13_practice.java
+++ src/edu/NTUNHS/luckypig/CH03_13_practice.java
@@ -2,8 +2,9 @@ package edu.NTUNHS.luckypig;
 
 public class CH03_13_practice {
     public static void main(String[] args) {
-        for(int i=1;i<100;i++){
-            if(i%3 == 0)System.out.println(i);
+        for(int i=1;i<=100;i++){
+            if(i%3 != 0)continue;
+            System.out.println(i);
         }
     }
 }
