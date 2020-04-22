bb3ef3495f5ead7505901b200532011966fdaa57
郵差 <a34007024@gmail.com>
2020-04-22 12:36:36 +0800

    PT6_1 and HW5 structure

---
+++ src/edu/NTUNHS/luckypig/PT6_1.java
@@ -0,0 +1,9 @@
+package edu.NTUNHS.luckypig;
+
+
+public class PT6_1 {
+    public static void main(String[] args) {
+        int[]  math  =  {50,  65,  80,  48,  35,  93,  57,  90,  86,  77};
+        
+    }
+}
