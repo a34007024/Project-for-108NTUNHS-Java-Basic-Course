6711ffbf16600dafc74d066ef791ce6390b1662c
郵差 <a34007024@gmail.com>
2020-04-25 22:35:16 +0800

    auto format file

--- src/edu/NTUNHS/luckypig/HW5.java
+++ src/edu/NTUNHS/luckypig/HW5.java
@@ -2,11 +2,7 @@ package edu.NTUNHS.luckypig;
 
 public class HW5 {
     public static void main(String[] args) {
-        //int[][] score = new int[3][5];
-        int[][] score={{76,80,66,},
-                {88,92,35},
-                {90,94,95},
-                {100,98,78},
-                {68,70,56}};
+        // int[][] score = new int[3][5];
+        int[][] score = { { 76, 80, 66, }, { 88, 92, 35 }, { 90, 94, 95 }, { 100, 98, 78 }, { 68, 70, 56 } };
     }
 }
