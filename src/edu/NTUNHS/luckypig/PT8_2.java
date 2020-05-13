1fb7f2405618a41e150485c3b3e3360059a7c4d5
郵差 <a34007024@gmail.com>
2020-05-13 16:18:25 +0800

    fix letter z and Z error
    z -> a
    Z -> A

--- src/edu/NTUNHS/luckypig/PT8_2.java
+++ src/edu/NTUNHS/luckypig/PT8_2.java
@@ -11,8 +11,13 @@ public class PT8_2 {
         String cyptoStr = "";
         sc.close();
         for (int i = 0; i < chArray.length; i++) {
-            chArray[i] = (char)((int)chArray[i] + 1);
-            //https://stackoverflow.com/questions/17984975/convert-int-to-char-in-java
+            if ((int) chArray[i] == 90)//Z
+                chArray[i] = (char)65;
+            else if ((int) chArray[i] == 122)//z
+                chArray[i] = (char)97;
+            else
+                chArray[i] = (char) ((int) chArray[i] + 1);
+            // https://stackoverflow.com/questions/17984975/convert-int-to-char-in-java
             cyptoStr += chArray[i];
         }
         System.out.println("加密過後的字串為:");
