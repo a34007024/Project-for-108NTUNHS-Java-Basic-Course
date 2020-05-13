e29c7404691b85066cf8d77ee487f4a5eaeb4c0e
郵差 <a34007024@gmail.com>
2020-05-13 17:02:54 +0800

    make encrypt section to public static function

--- src/edu/NTUNHS/luckypig/PT8_2.java
+++ src/edu/NTUNHS/luckypig/PT8_2.java
@@ -7,9 +7,13 @@ public class PT8_2 {
         System.out.println("請輸入一串英文字:");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        char[] chArray = input.toCharArray();
-        String cyptoStr = "";
         sc.close();
+        System.out.println("加密過後的字串為:");
+        System.out.println(caeserCipher(input));
+    }
+    public static String caeserCipher(String input){
+        char[] chArray = input.toCharArray();
+        String cipherTextStr = "";
         for (int i = 0; i < chArray.length; i++) {
             if ((int) chArray[i] == 90)//Z
                 chArray[i] = (char)65;
@@ -18,9 +22,8 @@ public class PT8_2 {
             else
                 chArray[i] = (char) ((int) chArray[i] + 1);
             // https://stackoverflow.com/questions/17984975/convert-int-to-char-in-java
-            cyptoStr += chArray[i];
+            cipherTextStr += chArray[i];
         }
-        System.out.println("加密過後的字串為:");
-        System.out.println(cyptoStr);
+        return cipherTextStr;
     }
 }
