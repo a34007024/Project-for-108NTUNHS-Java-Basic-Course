75edd80466c70cdf3734d25e12e8d7bf01839a39
郵差 <a34007024@gmail.com>
2020-06-03 16:22:47 +0800

    finish HW9_1 (guess number)

--- src/edu/NTUNHS/luckypig/HW9_1.java
+++ src/edu/NTUNHS/luckypig/HW9_1.java
@@ -1,7 +1,30 @@
 package edu.NTUNHS.luckypig;
 
+import java.util.Scanner;
+
 public class HW9_1 {
     public static void main(String[] args) {
-        //to-do use Guess to play guess number game
+        Guess guessNumGame = new Guess();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("來玩猜數字~~請輸入0~100的數字(輸入-1可以直接顯示答案):");
+        int guessNumber = sc1.nextInt();
+        int guessCount = 1;
+        while (guessNumber != -1) {
+            if (guessNumGame.judge(guessNumber) == -1)
+                System.out.println("再猜大一點");
+            else if (guessNumGame.judge(guessNumber) == 1)
+                System.out.println("再猜小一點");
+            else {
+                System.out.println("ヾ(≧▽≦*) 恭喜猜對~!");
+                break;
+            }
+            guessNumber = sc1.nextInt();
+            guessCount += 1;
+        }
+        sc1.close();
+        if (guessNumber == -1)
+            System.out.println("答案為:" + guessNumGame.getBingoNumber());
+        else
+            System.out.println("總共猜了" + guessCount + "次");
     }
 }
