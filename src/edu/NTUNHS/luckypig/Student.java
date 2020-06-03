8230c1119b0b0e62aadcd558ad29d1366f253eed
郵差 <a34007024@gmail.com>
2020-06-03 16:49:13 +0800

    practice extends (Student extends Person)

--- src/edu/NTUNHS/luckypig/Student.java
+++ src/edu/NTUNHS/luckypig/Student.java
@@ -1,5 +1,29 @@
 package edu.NTUNHS.luckypig;
 
 public class Student extends Person {
-    //to-do add constructor
+    private double chineseScore;
+    private double englishScore;
+    private double mathScore;
+
+    Student() {
+
+    }
+
+    Student(double chScore, double enScore, double mathSC) {
+        this.chineseScore = chScore;
+        this.englishScore = enScore;
+        this.mathScore = mathSC;
+    }
+
+    public double getSum() {
+        return chineseScore + englishScore + mathScore;
+    }
+
+    public double getAverage() {
+        return (chineseScore + englishScore + mathScore) / 3;
+    }
+
+    public void printSum(){
+        System.out.println(name + "的總分為:"+ getSum() +"分");
+    }
 }
---
+++ src/edu/NTUNHS/luckypig/playGround.java
@@ -0,0 +1,8 @@
+package edu.NTUNHS.luckypig;
+
+public class playGround {
+    public static void main(String[] args) {
+        Student s1 = new Student(100, 66, 88);
+        s1.printSum();
+    }
+}
