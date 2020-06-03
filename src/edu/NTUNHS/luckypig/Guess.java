a2ca8248042ea534487bf345e228ad7b4f44f000
郵差 <a34007024@gmail.com>
2020-06-03 14:48:52 +0800

    Update Guess.java

--- src/edu/NTUNHS/luckypig/Guess.java
+++ src/edu/NTUNHS/luckypig/Guess.java
@@ -1,5 +1,30 @@
 package edu.NTUNHS.luckypig;
 
+import java.util.Random;
+
 public class Guess {
-    //to-do guess number game~~
+    private int bingoNumber;
+
+    Guess() {
+        Random rnd = new Random();
+        bingoNumber = rnd.nextInt(101);// return number between 0~100
+    }
+
+    Guess(int minNumber, int MaxNumber) {
+        Random rnd = new Random();
+        bingoNumber = minNumber + rnd.nextInt(MaxNumber + 1 - minNumber);
+    }
+
+    public int judge(int guessNumber) {
+        if (guessNumber == bingoNumber)
+            return 0;
+        else if (guessNumber > bingoNumber)
+            return 1;
+        else
+            return -1;
+    }
+
+    public int getBingoNumber(){
+        return bingoNumber;
+    }
 }
